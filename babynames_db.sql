a25bb739be56de7866a9e11b948b6832ea593a2d
onenaghi <onenaghi@yahoo.com>
2020-09-10 19:14:33 -0500

    New Database Structure

--- babynames_db.sql
+++ babynames_db.sql
@@ -3,12 +3,25 @@ DROP TABLE IF EXISTS baby_names;
 
 -- Create Table
 CREATE TABLE baby_names (
- 	id int unique,
+ 	id int Primary Key,
 	state text,
 	sex varchar(1),
 	year int,
 	name text,
-	number int,
+	number int
 );
 
+-- View all the Data
 select * from baby_names;
+
+--Example - Finding the Top 10 years with most 'Robert's born in Texas
+SELECT year, number
+FROM baby_names
+WHERE name='Robert' and state='TX' and sex='M'
+ORDER BY number DESC
+LIMIT(10)
+;
+
+
+
+
