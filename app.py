615efc953a598c4cbc85db79302835a61d45960a
Dany Koudede <koudededany@icloud.com>
2020-09-15 16:20:44 -0700

    new commit

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
 # import necessary libraries
-import pandas as pd
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, inspect, join, outerjoin, MetaData, Table
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
+# Flask Setup
 app = Flask(__name__)
 
-  
-app.config['SQLALCHEMY_DATABASE_URL']= os.environ.get('postgresql://postgres:Koudede$89@localhost:5432/babynames_db')
 
+# baby_names= base.classes.baby_names
+
+from flask_sqlalchemy import SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:Koudede$89@localhost:5432/babynames_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save references to each table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
+# reflect the existing base into a new model 
+base=automap_base()
+#reflect the table 
+base.prepare(engine, reflect=True)
+#save reference to the table 
+baby_names= create_classes(db)
 
 
 # create route that renders index.html template
@@ -37,9 +34,36 @@ session = Session(engine)
 def home():
     return render_template("index.html")
 
-@app.route("/api/v1.0/data")
-def table_data():
+@app.route("/api/map")
+def data():
 
+return render_template("map.html")
+
+@app.route("/api/graph ")
+def data():
+    results = db.session.query(names.Name, names.Number).all()
+
+    hover_text = [result[0] for result in results]
+    Number = [result[1] for result in results]
+    year = [result[2] for result in results]
+    names_data = [{
+        "type": "scattergeo",
+        "locationmode": "USA-states",
+        "Number": Number,
+        "year": year,
+        "text": hover_text,
+        "hoverinfo": "text",
+        "marker": {
+            "size": 50,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
+    }]
+
+    return jsonify(Names_data)
 if __name__ == "__main__":
     app.debug =True
     app.run()
+
